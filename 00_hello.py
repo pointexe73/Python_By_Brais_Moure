954bbe0d3495a0ca03bfe52691ea13c5cd87717c
pointexe73 <mejluis98@gmail.com>
2024-08-26 07:19:02 -0400

    Clase Hechas

---
+++ 00_hello.py
@@ -0,0 +1,25 @@
+# Esto es un comentario
+# Nuestro hola mundo en Python
+
+print("Hola Python")
+print(('Hola Python'))
+print()
+
+"""
+Este es un 
+comentario
+en varias lineas
+"""
+'''
+Este es un  
+comentario con comillas simples
+en varias lineas
+'''
+# Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5))                 # Tipo 'int'
+print(type(3.14))              # Tipo 'float'
+print(type(3 + 1j))            # Tipo 'complex'
+print(type(True))              # Tipo 'bool
+
+print(type(print("Mi cadena de texto"))) # Tipo 'NoneType'
---
+++ 01_variables.py
@@ -0,0 +1,47 @@
+# VARIABLES
+
+my_string_variable = "My String Variable"
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+my_bool_variable =  False
+print(my_bool_variable)
+
+# Concatenacion de variaables en un print
+print(my_int_variable,  my_int_to_str_variable, my_bool_variable)
+print("Este es el valor de:", my_bool_variable)
+
+# Una funcion del sistema
+print(len(my_string_variable))
+
+# Variables en una sola linea. ¡Cuidado con abusar de esta sintaxis!
+name, surname, alias, age = "Juan", "Diaz", "Juandc", 35
+print("Me llamo:", name, surname, ". Mi edad es:", age, ". Y mi alias es:", alias)
+
+# Inputs
+
+name = input('What is your name: ')
+age = input('How old are you? ')
+print(name)
+print(age)
+
+# Cambiamos su tipo
+name = 35
+age = "Juan"
+print(name)
+print(age)
+
+
+# Forzamos el tipo?
+address: str = "Mi direccion"
+address = 32
+address = True
+address = 5
+address = 1.2
+print(type(address))
