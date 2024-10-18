e9455efdd9da224212b8abffc32b2d771f34c473
pointexe73 <mejluis98@gmail.com>
2024-10-18 13:23:23 -0400

    Clase Completada

--- 12_exceptions.py
+++ 12_exceptions.py
@@ -3,11 +3,11 @@
 print("1 (try-except)")
 print("---------------------")
 numberOne = 5
-numeberTwo = "1"
+numberTwo = "1"
 
 # -c -> Se ejecuta except
 try:
-    print(numberOne + numeberTwo)
+    print(numberOne + numberTwo)
     print("No se ha producido un error")
     
 except:
@@ -45,7 +45,7 @@ numberSix = "1"
 # -c -> Se ejecuta except y finally siempre se ejecuta
 try:
     # -c -> No se ejecuta
-    print(numberOne + numeberTwo)
+    print(numberOne + numberTwo)
     print("No se ha producido un error")
     
 except :
@@ -62,3 +62,36 @@ finally:
     # -c -> Se ejecuta siempre
     print("La ejecucion continua")
     # -r -> La ejecucion continua
+    
+    
+print()
+print("4 (Excepcition for type)")
+print("---------------------")
+
+# Excepciones por tipo 
+
+try:
+    print(numberOne + numberTwo)
+    print("No se ha producido un error")
+    
+except ValueError:
+    print("Se ha producido un ValueError")
+except TypeError:
+    print("Se ha producido un TypeError")
+    
+    
+print()
+print("5 (capturning exception information)")
+print("---------------------")
+
+# Capturar la informacion de la excepcion
+
+try:
+    print(numberOne + numberTwo)
+    print("No se ha producido un error")
+
+except ValueError as error:\
+    print(error)
+    
+except Exception as my_random_error_name:
+    print(my_random_error_name)
